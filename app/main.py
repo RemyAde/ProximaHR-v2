2918a04512dea41d31b0dd8c3917908c8f6cdf37
Aderemi Samuel <remysamade@gmail.com>
2024-11-08 15:00:03 +0100

    Update main.py
    add origins

--- app/main.py
+++ app/main.py
@@ -13,7 +13,11 @@ app.include_router(employee.router, prefix="/employee", tags=["employee"])
 # Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [""],
+    allow_origins = [
+"https://hrm-project-rosy.vercel.app",
+"http://localhost:3000",
+"http://127.0.0.1:3000"
+],
     allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
