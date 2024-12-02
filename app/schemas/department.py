024c0ca63361308790876bd408df1759de06a34c
RemyAde <remysamade@gmail.com>
2024-12-03 00:33:42 +0100

    added documentation for department creation schema

--- app/schemas/department.py
+++ app/schemas/department.py
@@ -1,7 +1,18 @@
-from typing import Optional
-from pydantic import BaseModel
+from typing import Optional, List
+from pydantic import BaseModel, Field
 
 
 class DepartmentCreate(BaseModel):
-    name: str
-    hod: Optional[str] = None
+    name: str = Field(..., description="Name of the department.")
+    hod: Optional[str] = Field(
+        None, 
+        description="The employee ID of the Head of Department (HOD)."
+    )
+    staffs: Optional[List[str]] = Field(
+        default_factory=list, 
+        description="List of employee IDs representing the staff members in the department."
+    )
+    description: Optional[str] = Field(
+        None, 
+        description="A brief description of the department."
+    )
