ebda36fcdb07bab88fe86db96ec6a601b7ccf70b
RemyAde <remysamade@gmail.com>
2025-01-28 23:25:42 +0100

    feat: add new local development URL to CORS configuration

--- app/main.py
+++ app/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = [
         "https://hrm-project-rosy.vercel.app",
+        "https://masterhrmproject.vercel.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
